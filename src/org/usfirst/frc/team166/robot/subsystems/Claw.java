df91bd528278eb803d876aef832691de67752093
LoganNo <lnozell@gmail.com>
2015-01-18 16:35:53 -0500

    Added prelim outline of subsystems

---
+++ src/org/usfirst/frc/team166/robot/subsystems/Claw.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Claw extends Subsystem {
+	
+	public Claw(Solenoid solenoid){
+		
+	}
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public void open(){
+		
+	return;
+	}
+	public void close(){
+		
+	return;	
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	
+    }
+}
+
---
+++ src/org/usfirst/frc/team166/robot/subsystems/Wing.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Wing extends Subsystem {
+	
+	public Wing(Solenoid solenoid) {
+		
+	}
+	
+	public void raise(){
+		
+	return;	
+	}
+	public void lower(){
+		
+	return;	
+	}
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
