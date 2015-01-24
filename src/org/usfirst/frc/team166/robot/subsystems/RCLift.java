85964c1d041e515252db64b6c06a141324bd211a
danmulligan <danmulligan133@gmail.com>
2015-01-23 19:48:42 -0500

    Separated Lift into ToteLift and RCLift.

---
+++ src/org/usfirst/frc/team166/robot/subsystems/RCLift.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team166.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class RCLift extends Subsystem {
+	public RCLift(SpeedController motor) {
+
+	}
+
+	public void raise() {
+
+		return;
+	}
+
+	public void lower() {
+
+		return;
+	}
+
+	public void stop() {
+
+		return;
+	}
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
