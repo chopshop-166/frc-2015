dd0a6cad83fa0a08cfa7d6be578ab8422b9dece0
Logan Brancheau <logan.brancheau@gmail.com>
2015-01-31 09:56:47 -0500

    Removed Explicit Requires
    Command groups already have all the requires of the commands it uses,
    and added comments saying so, and auto formatted to delete excess import

--- src/org/usfirst/frc/team166/robot/commands/LowerWings.java
+++ src/org/usfirst/frc/team166/robot/commands/LowerWings.java
@@ -2,17 +2,13 @@ package org.usfirst.frc.team166.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-import org.usfirst.frc.team166.robot.Robot;
-
 /**
  * Command group for lowering wings
  */
 public class LowerWings extends CommandGroup {
 
 	public LowerWings() {
-		// wings to be lowered
-		requires(Robot.leftWing);
-		requires(Robot.rightWing);
+		// command groups don't need explicit requires
 		// starts lowering both wings at the same time
 		addParallel(new LowerLeftWing());
 		addSequential(new LowerRightWing());
