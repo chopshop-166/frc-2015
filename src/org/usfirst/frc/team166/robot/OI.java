18a12bdd685e5c3e0dc70abc2e835416146dd5d2
Austin <austin.brancheau@gmail.com>
2015-01-31 10:37:10 -0500

    Adds button on SmartDashboard for LiftWings
    adds the LiftgWings command group to the smart dashboard as a clickable
    button for testing.

--- src/org/usfirst/frc/team166/robot/OI.java
+++ src/org/usfirst/frc/team166/robot/OI.java
@@ -2,10 +2,12 @@ package org.usfirst.frc.team166.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team166.robot.commands.Autonomous;
 import org.usfirst.frc.team166.robot.commands.CancelDriveCommand;
 import org.usfirst.frc.team166.robot.commands.DriveDirection;
+import org.usfirst.frc.team166.robot.commands.LiftWings;
 
 /**
  * This class is the glue that binds the controls on the physical operator interface to the commands and command groups
@@ -24,6 +26,7 @@ public class OI {
 		button2.whenPressed(new CancelDriveCommand());
 		button3.whileHeld(new DriveDirection(90));
 		button4.whenPressed(new Autonomous());
+		SmartDashboard.putData("LiftWings", new LiftWings());
 	}
 
 	public Joystick getDriveJoystick() {
