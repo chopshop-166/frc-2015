0904615c89f3310da8dcc505f1f685f34d02cd48
atrimper <liv4waterville@gmail.com>
2015-03-06 18:42:32 -0500

    I'm dumb

---
+++ src/org/usfirst/frc/team166/robot/commands/autonomous/RotateLeft45.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team166.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team166.robot.Robot;
+
+/**
+ *
+ */
+public class RotateLeft45 extends Command {
+
+	public RotateLeft45() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.drive);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.drive.turn45Left();
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return (-35 > Robot.drive.getGyro());
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
